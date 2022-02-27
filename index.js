2615bff3981ed87cf7cbfd50e159076f2f1b0462
Devanarayan J <devan23.5.2001@gmail.com>
2022-02-27 13:46:45 +0530

    added local variable to nagigate between store and library after logging in

--- index.js
+++ index.js
@@ -8,6 +8,16 @@ app.use(express.static("public"));
 var path = require("path");
 app.use(express.json()); // to support JSON-encoded bodies
 app.use(express.urlencoded({ extended: true }));
+var session = require("express-session");
+app.set("trust proxy", 1); // trust first proxy
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true },
+  })
+);
 
 const sqlite3 = require("sqlite3").verbose();
 const db_name = path.join(__dirname, "data", "apptest.db");
@@ -38,8 +48,8 @@ const db = new sqlite3.Database(db_name, (err) => {
 
 app.post("/signup", (req, res) => {
   console.log(req.body);
-  let username = req.body.username;
-  let password = req.body.password;
+  let username = app.locals.u;
+  let password = app.locals.p;
   let email = req.body.email;
 
   const sql_q = `INSERT INTO Account (username, password, email) VALUES (?, ?, ?)`;
@@ -88,6 +98,26 @@ app.get("/", (req, res) => {
 app.get("/about", (req, res) => {
   res.render("about");
 });
+app.get("/library", (req, res) => {
+  console.log("inside sess", app.locals.u, app.locals.p);
+  // console.log(` ${row.username} - ${row.password}`);
+  let u_id = undefined;
+  const sql_query2 = `SELECT acc_id from account where username="${app.locals.u}";`;
+  const sql_query3 = `SELECT * from game where game_id in (SELECT game_id from plays where acc_id=?)`;
+  db.get(sql_query2, [], (err, tup) => {
+    if (err) {
+      throw err;
+    }
+    u_id = tup.acc_id;
+    console.log(`u_id is ${u_id}`);
+    db.all(sql_query3, [u_id], (err, rows) => {
+      if (err) {
+        return console.error(err.message);
+      }
+      res.render("library", { model: rows });
+    });
+  });
+});
 
 app.get("/data", (req, res) => {
   const test = {
@@ -101,6 +131,12 @@ app.post("/library", (req, res) => {
   console.log("req body is", req.body);
   let username = req.body.username;
   let password = req.body.password;
+  app.locals.u = username;
+  app.locals.p = password;
+  req.session.authenticate = true;
+  req.session.username = username;
+  req.session.password = password;
+  console.log("req sess username is ", app.locals.u);
   console.log("console log for var paramaters", username, password);
   var a = 0;
 
@@ -116,8 +152,9 @@ app.post("/library", (req, res) => {
         throw err;
       }
       if (row) {
-        //req.session.authenticate = true;
-        //req.session.username = username;
+        req.session.authenticate = true;
+        req.session.username = username;
+        req.session.password = password;
         console.log(` ${row.username} - ${row.password}`);
         let u_id = undefined;
         const sql_query2 = `SELECT acc_id from account where username="${row.username}";`;
